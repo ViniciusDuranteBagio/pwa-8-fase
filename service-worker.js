b8518c4b81eca912bdc1cd4ef9917e3f3f65dcab
Bruno Fernandes <brunofernandes0330@gmail.com>
2025-08-11 20:58:12 -0300

    Primeiro Commit

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@ self.addEventListener('install', event => {
     caches.open('v1').then(cache => {
       return cache.addAll([
         '/index.html',
-        '/icon1.png',
+        '/iconteste.png',
         '/manifest.json'
       ]);
     })
