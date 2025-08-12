2515a4b14e6221ce2540f3e407b3d95aa1ae6831
wesley <wesleyrab2020@gmail.com>
2025-08-11 21:20:32 -0300

    vamp

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,20 @@
-self.addEventListener('install', event => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open('v1').then(cache => {
+    caches.open("v1").then((cache) => {
       return cache.addAll([
-        '/index.html',
-        '/icon1.png',
-        '/manifest.json'
+        "/index.html",
+        "/vamp-192.png",
+        "/vamp-512.png",
+        "/manifest.json",
       ]);
     })
   );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    caches
+      .match(event.request)
+      .then((response) => response || fetch(event.request))
   );
 });
