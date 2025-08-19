17a26f8750576aec61b6b756281d13007004ab02
RenatoBonettiNeto <bonettineto.renato03@gmail.com>
2025-08-18 21:16:22 -0300

    feat: Excluido arquivos da 3a-aula (18-08-25)

--- todo-react/src/App.jsx
+++ todo-react/src/App.jsx
@@ -2,21 +2,11 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Ola from './Ola.jsx'
-import Lista from './Lista.jsx'
 
 function App() {
-  const nome = "Renato"
 
   return (
     <>
-      <div>
-        <Ola nome='Renato'></Ola>
-        <Ola nome='Bonetti'></Ola>
-      </div>
-      <div>
-        <Lista></Lista>
-      </div>
     </>
   )
 }
